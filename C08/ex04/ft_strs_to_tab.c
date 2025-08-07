fbe1a07c6eed8a0476b945fba08e6c0ae7b014d2
keroxi <keroxi@outlook.in>
2025-08-07 09:50:10 +0100

    C08

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -6,10 +6,67 @@
 /*   By: aasita <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 21:34:03 by aasita            #+#    #+#             */
-/*   Updated: 2025/08/05 21:37:08 by aasita           ###   ########.fr       */
+/*   Updated: 2025/08/07 05:30:06 by aasita           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "ft_stock_str.h"
 
+int	ft_strlen(char *str)
+{
+	int	len;
 
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		len;
+	char	*dup;
+
+	len = ft_strlen(src);
+	dup = malloc(len + 1);
+	if (!dup)
+		return (NULL);
+	ft_strcpy(dup, src);
+	return (dup);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int					i;
+	struct s_stock_str	*str_tab;
+
+	i = 0;
+	str_tab = malloc(sizeof(struct s_stock_str) * (ac + 1));
+	if (!str_tab)
+		return (NULL);
+	while (i < ac)
+	{
+		str_tab[i].size = ft_strlen(av[i]);
+		str_tab[i].str = av[i];
+		str_tab[i].copy = ft_strdup(av[i]);
+		i++;
+	}
+	str_tab[i].str = 0;
+	str_tab[i].copy = 0;
+	return (str_tab);
+}
---
+++ C08/ex05/ft_show_tab.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aasita <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/07 05:04:30 by aasita            #+#    #+#             */
+/*   Updated: 2025/08/07 05:35:49 by aasita           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+		ft_putstr("-2147483648");
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].str != 0)
+	{
+		ft_putstr(par[i].str);
+		ft_putchar('\n');
+		ft_putnbr(par[i].size);
+		ft_putchar('\n');
+		ft_putstr(par[i].copy);
+		ft_putchar('\n');
+		i++;
+	}
+}
